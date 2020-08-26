ed24b97394a8f6debb2ff6b75fff3929d5df7a06
codecademy <ccuser@codecademy.com>
2020-08-26 04:19:38 +0000

    Created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -1,21 +1,53 @@
 destinations=["Paris, France","Shanghai, China","Los Angeles, USA","São Paulo, Brazil","Cairo, Egypt"]
 test_traveler=['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
-destination_index=[]
-def get_destination_index(destination):
-  
-    a=destinations.index(destination)
 
-    
-    destination_index.append(a)
-    return destination_index  
+def get_destination_index(destination):
+  destination_index=destinations.index(destination)
+  return destination_index  
 
 def get_traveler_location(traveler):
   traveler_destination=traveler[1]
   traveler_destination_index=get_destination_index(traveler_destination)
   return traveler_destination_index
 
-test_destination_index=get_traveler_location(test_traveler)  
-print(test_destination_index)
+
+#print(test_destination_index)
+
+attractions=[[] for destination in range(len(destinations))]
+#print(attractions)
+def add_attraction(destination,attraction):
+  
+  try:
+    destination_index=get_destination_index(destination)
+    attractions_for_destination=attractions[destination_index]
+    attractions_for_destination.append(attraction)
+    return attractions_for_destination
+
+  except ValueError:
+    print("destination is not listed here")
+    return 
+  
+  
+  
+#test_destination_index=get_traveler_location(test_traveler) 
+
+#print(attractions)
+
+add_attraction("Los Angeles, USA" ,['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+    
+
+
+
 
 
 
